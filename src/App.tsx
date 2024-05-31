fb5f7a00d9e526b16a809cf393664715557a0a10
sulenq <whyimustwriteit@gmail.com>
2024-05-31 15:11:41 +0700

    tes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { DndContext } from "@dnd-kit/core";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./globalStyle.css";
 import Landing from "./pages/Landing";
@@ -8,21 +7,17 @@ import globalTheme from "./theme/globalTheme";
 import MissingPage from "./pages/errors/MissingPage";
 import Foto from "./pages/Foto";
 import Edit from "./pages/Edit";
-import Tes from "./pages/Tes";
 
 export const App = () => (
-  <DndContext>
-    <ChakraProvider theme={globalTheme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/foto" element={<Foto />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/tes" element={<Tes />} />
-          <Route path="/*" element={<MissingPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
-  </DndContext>
+  <ChakraProvider theme={globalTheme}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/foto" element={<Foto />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/*" element={<MissingPage />} />
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>
 );
